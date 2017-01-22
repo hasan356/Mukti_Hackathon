27d9b8a51eac01ad4326348bf0b43f2ed850ee47
root <hasan.150295@gmail.com>
2017-01-22 17:15:19 +0530

    add new features

--- hasan.py
+++ hasan.py
@@ -4,7 +4,11 @@ import requests
 import wget
 import shutil
 import os.path
-os.chdir('/home/hasan/Student_Notice_Board')
+dir=raw_input("Enter the path of directory")
+os.chdir(dir)
+print 'Enter 1 to download all pdf'
+print "Enter 2 to check new updates in Student board"
+c=int(input("Enter a number")) 
 site="http://nitdgp.ac.in/Student_Notice_Board.php"
 page = urllib.urlopen(site)
 soup=BeautifulSoup(page,'lxml')
@@ -14,17 +18,23 @@ links=soup.find_all("a")
 list_of_all_names_of_pdf=[]
 start='http://nitdgp.ac.in/'
 save_path = '/home/Student_Notice_Board/'
-for link in links:
-	url=link.get("href")
-	length=len(url)
-	if(url[length-1]=='f'):
-		name_of_file=link.text
-		completeName = os.path.join(save_path, name_of_file) 
-		list_of_all_names_of_pdf.append(link.text)
-		wget.download(start+url)
+if(c==1):
+	for link in links:
+		url=link.get("href")
+		length=len(url)
+		if(url[length-1]=='f'):
+			list_of_all_names_of_pdf.append(link.text)
+			wget.download(start+url)
 
-
-		
+else:
+	for link in links:
+		url=link.get("href")
+		length=len(url)
+		if(url[length-1]=='f'):
+			for names in list_of_all_names_of_pdf:
+				if(link.text not in names):
+					list_of_all_names_of_pdf.append(link.text)
+					wget.download(start+url)
 		
 
 
